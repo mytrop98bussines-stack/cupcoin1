a9fcf067707b7a5bf1e6420fdfd6f4e4ec828279
mytrop98bussines-stack <mytrop98bussines@gmail.com>
2026-07-31 09:29:46 -0400

    Update firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,14 +8,13 @@ importScripts('https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-comp
 
 // ─── Configuración Firebase ──────────────────────────────
 firebase.initializeApp({
-  apiKey:            "REPLACE_API_KEY",
-  authDomain:        "REPLACE_AUTH_DOMAIN",
-  projectId:         "REPLACE_PROJECT_ID",
-  storageBucket:     "REPLACE_STORAGE_BUCKET",
-  messagingSenderId: "REPLACE_MESSAGING_SENDER_ID",
-  appId:             "REPLACE_APP_ID",
-});
-
+    apiKey:            import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain:        import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId:         import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket:     import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId:             import.meta.env.VITE_FIREBASE_APP_ID,
+};
 const messaging = firebase.messaging();
 
 // ═══════════════════════════════════════════════════════════
